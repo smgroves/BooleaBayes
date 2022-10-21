ae489bf4dfa98ec6f1e3ca84d89c273ceceab9c3
smgroves <sarahmaddoxgroves@gmail.com>
2022-10-21 09:38:33 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ readme = open("README.rst").read()
 
 setup(
     name="booleabayes",
-    version="0.0.6",
+    version="0.0.7",
     description="A suite for network inference from transcriptomics data",
     long_description=readme,
     long_description_content_type="text/x-rst",
